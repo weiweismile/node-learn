a86cf1f249aa09153e8448fb0b59345835bba929
fanweiwei <fanweiwei@wezhuiyi.com>
2018-12-17 00:25:02 +0800

    add 项目打包demo

---
+++ demo/build-project-files/build.js
@@ -0,0 +1,18 @@
+
+<html>
+	<head></head>
+	<body>
+		<h1>node学习</h1>
+	</body>
+</html>
+1111111fwfwfw2222 4444dwdwdw  77777  9999
+3333399999
+
+<html>
+	<head></head>
+	<body>
+		<h1>node学习</h1>
+	</body>
+</html>
+1111111fwfwfw2222 4444dwdwdw  77777  9999
+3333399999
--- demo/project-build.js
+++ demo/project-build.js
@@ -22,11 +22,20 @@ const options = {
 };
 
 const fs = require('fs');
+
+// 自动构建项目代码
 function buildProject(options) {
     if (!options.project) return;
     // 创建目录就是创建一个文件夹
-    fs.mkdirSync(options.project);
-    console.log('创建'+ options.project + '目录成功');
+    try {
+       fs.mkdirSync(options.project);   
+       console.log('创建'+ options.project + '目录成功');
+    } catch (error) {
+        if (error.code === 'EEXIST') {
+            console.log('该项目已经创建');
+        }
+        return;
+    }
     const files = options.files || [];
     files.forEach(file => {
         const filePath = './'+ options.project + '/' + file.name;
@@ -40,4 +49,48 @@ function buildProject(options) {
     });
 }
 
+
+// 自动打包工具,demo只展示监听js文件夹下的所有的文件，一个文件发生变化，就重新打包所有内容
+
+function autoBuildProject(projectPath) {
+    // 1、读取projectPath目录下的所有的文件
+    const buildPath = projectPath + '/build.js';
+    fs.readdir(projectPath, function(err, files) {
+        files.forEach(file => {
+            const filePath = projectPath + '/' + file;
+            const isDirectory = fs.statSync(filePath).isDirectory();
+            if (isDirectory) {
+                 // 2、监听每一个文件夹（新增文件的时候就会触发事件）
+                fs.watch(filePath, function(event, file) {
+                    // 3、一旦某一个文件发生变化，就将所有文件内容读取一遍
+                    const content = readEachFile(projectPath);
+                    console.log(content, 'content');
+                    fs.writeFileSync(buildPath, content);
+                });
+            }
+        });
+    });
+}
+
+
+function readEachFile(dir) {
+    let content = '';
+    const handler = function(path) {
+        fs.readdirSync(path).forEach(file => {
+                const filePath = path + '/' + file;
+                if(fs.statSync(filePath).isFile()) {
+                    content += fs.readFileSync(filePath).toString() + '\n';
+                } else {
+                    handler(filePath);
+                }
+            });
+    }
+    handler(dir);
+    return content;
+}
+
+// 注意同步和异步，之前使用的异步使得content在返回时没有数据。
+
+
 buildProject(options);
+autoBuildProject(options.project);
