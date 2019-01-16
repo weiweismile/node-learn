8006050d17b5d9024ef398c843b380e317faf615
fanweiwei <fanweiwei@wezhuiyi.com>
2019-01-16 21:53:33 +0800

    豆瓣爬虫

--- 爬虫学习/index.js
+++ 爬虫学习/index.js
@@ -1,28 +1,48 @@
+// 爬一张图片
+// const https = require('https');
+// const fs = require('fs');
+
+// const arr=[];
+// let str = '';
+
+// let req = https.request({
+//     'protocol': 'https:',
+//     'hostname': 'ss1.bdstatic.com',
+//     'path': '/kvoZeXSm1A5BphGlnYG/skin/120.jpg?2',
+// }, res => {
+//     console.log(res, 7777);
+//     res.on('data', function(chunk) {
+//         console.log(chunk, 888);
+//         arr.push(chunk);
+//         str += chunk;
+//     });
+//     res.on('end', function() {
+//         let b = Buffer.concat(arr);
+//         // buffer二进制数组，转换成一个真正的buffer对象。
+//         fs.writeFile('test.jpg', b);
+//     });
+// });
+// req.end();
+
+// 爬豆瓣
+const JSDOM = require('jsdom').JSDOM;
 const https = require('https');
-const fs = require('fs');
-
-const arr=[];
-let str = '';
-
+const arr = [];
+let str='';
 let req = https.request({
     'protocol': 'https:',
-    'hostname': 'ss1.bdstatic.com',
-    'path': '/kvoZeXSm1A5BphGlnYG/skin/120.jpg?2',
+    'hostname': 'movie.douban.com',
+    'path': '/subject/26394152/?from=showing',
 }, res => {
-    console.log(res, 7777);
     res.on('data', function(chunk) {
-        console.log(chunk, 888);
         arr.push(chunk);
         str += chunk;
+        // console.log(str, 8989);
     });
     res.on('end', function() {
-        let b = Buffer.concat(arr);
-        // 将其转换成一个真正的buffer对象。
-        fs.writeFile('test.jpg', b);
+        const dom = new JSDOM(str);
+        console.log(dom.window.document.querySelector('h1').textContent);
     });
 });
+
 req.end();
-
-
-
-// ?????二进制转换问题
